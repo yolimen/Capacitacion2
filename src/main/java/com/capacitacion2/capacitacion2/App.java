871031ebc785d5c5adaacc08117d79747733845d
yolimen <heidi.linares40@gmail.com>
2020-10-20 08:32:03 -0500

    Primer commit

---
+++ src/main/java/com/capacitacion2/capacitacion2/App.java
@@ -0,0 +1,21 @@
+package com.capacitacion2.capacitacion2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[opcion] args )
+    {
+        switch (opcion) {
+		case value:
+			
+			break;
+
+		default:
+			break;
+		}
+        
+    }
+
